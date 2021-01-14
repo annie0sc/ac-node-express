47d65483a36cbda249abc216887768c891e42f5e
annie0sc <annie0sc@gmail.com>
2021-01-14 16:53:34 -0600

    added express details.

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.send('Hello World - Annie App!')
 })
 
+app.get('/about', (req, res) => {
+    res.send("This is an example for node-express.")
+  })
+  
+  app.get('/contact', (req, res) => {
+    res.send("contact for help")
+  })
+  
+  app.get('/help/:id', (req, res) => {
+    res.send(`For any further assistance.. ${req.params.id}`)
+  })
+  
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
