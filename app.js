75f1d544190a956c689714592e062178193cdfe3
annie0sc <annie0sc@gmail.com>
2021-01-14 16:36:21 -0600

    Syntax error clearance.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World - Annie's App!')
+  res.send('Hello World - Annie App!')
 })
 
 app.listen(port, () => {
